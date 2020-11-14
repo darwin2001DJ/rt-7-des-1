f61bbcca8e43853a92f55bdb3eadf4f941ebb18e
Your Name <darwinmuthu450@gmail.com>
2020-11-14 08:56:10 +0530

    init and data added

---
+++ src/MovieJSON.js
@@ -0,0 +1,22 @@
+export let movies = [
+  {
+    name: "Bombshell",
+    src: require("./image/bombshell.jpg"),
+  },
+  {
+    name: "The Grudge",
+    src: require("./image/thegrudge.jpg"),
+  },
+  {
+    name: "Mafia",
+    src: require("./image/mafia.jpg"),
+  },
+  {
+    name: "Darbar",
+    src: require("./image/darbar.png"),
+  },
+  {
+    name: "Oh My Kadavuley",
+    src: require("./image/OMK.jpg"),
+  },
+];
